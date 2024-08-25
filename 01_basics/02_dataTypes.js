a4bf47f5dc5e4e6fb801e0c4b487efb57eb8180c
Sanskar Mishra <152903486+39sanskar@users.noreply.github.com>
2024-08-25 19:57:31 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as a newer version in a engine.
+
+//alert(3 + 3)// we are using nodejs , not browser
+
+console.log(3 + 3); console.log("Satyam") // code readability should be high.
+
+let name = "Aman";
+let age = 18
+let isLogged = false
+let state = null // for example in the case of temperature .
+// in case of temperature  0 is also is an temperature.
+
+// number => 2 to power 53
+// bigInt
+// string => "" (Empty string)
+// boolean => true/ false
+// null => standalone value in javascript type of value.(Empty value)
+// null it is an object.(interview question.)
+// undefined => value is not define variable declare but value is not define.
+// symbol => unique
+
+//object
+
+console.log(typeof "Sanskar");// String
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined(it is a type itself.)
+
+console.log(typeof null); //object
+
+
