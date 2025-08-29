5fcbe4f3f47ab99f317e9720a5c35b7e4015ca3e
39sanskar <mishrasanskar496@gmail.com>
2025-08-29 19:13:27 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";  // treat all JS code as newer version of Javascript 
+
+// alert("hello");  // we are using nodejs, not browser
+
+// console.log(3 + 3); console.log("Sanskar") // dont use this because of code readability
+
+console.log(3 + 3 );  // code readability should be high
+
+// documentation = tc39, mdn 
+
+let name = "Sanskar"
+let age = 18
+let isLoggedIn = false
+
+// Primitive dataTypes
+
+// number => range 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+
+// null => it is a standalone value, null is specially a value (empty), eg. you send a request from a server what is the temperature but their are some error so temperature is not avilable if he says temperature value is 0 (0 is also a value in the temperature) that's why  we prefer  null as a value which means empty.
+
+// undefined => when you are assign a variable but not assign any value to variable 
+
+// symbol => it uses in the react. for find uniqueness. 
+
+
+// Non-Primitive dataTypes
+
+// object 
+
+console.log(typeof "Sanskar");  // string
+console.log(typeof age);  // number
+console.log(typeof null); // object (interview question)
+console.log(typeof undefined); // undefined (special type)
+
