57ad2a2b9a1b3dcd219174d935afbe2bc3c38810
39sanskar <mishrasanskar496@gmail.com>
2025-08-29 18:38:34 +0530

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,31 @@
+const accountId = 144553 // it is not change 
+let accountEmail = "sanskar@gmail.com"
+var accountPassword = "12345"
+accountCity   = "Jaipur"
+let accountState;    // undefined because not assign any value 
+// accountId = 2  // not allow 
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); // show all thing inside the table.
+
+
+// in the javascript declaration of the constant using const keyword but the decaration of the variable 
+// there are two ways let and var 
+// {} => scope 
+/*
+Perefer:  not to use var
+because of issue in block scope and functional scope.
+*/
+
+/*
+
+// software has capability to understand the syntax, like .js , .py  etc 
+// node.js , deno.js => search founder name
+
+console.log("Sanskar Mishra");
+
+*/
