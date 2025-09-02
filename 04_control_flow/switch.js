e65aaf50be1bd01ce7f905f1dd6cc69f4c284b0f
39sanskar <mishrasanskar496@gmail.com>
2025-09-03 04:30:48 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,76 @@
+// switch (key) {
+//   case value:
+//       break;
+
+//   default:
+//     break;    
+
+// }
+
+const value = 3
+
+switch (value) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+} // eak baar jaha case match ho gaya uskae baad sara code execute karta hai except default.
+// so, apply break it break the control flow
+
+
+// Match month name as a String
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
